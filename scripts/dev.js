3dd96cbd922dfb20248bec6cf44518a88f1ace74
JaniceZhao22 <janicezhao22@163.com>
2022-05-17 17:04:49 +0800

    test

--- scripts/dev.js
+++ scripts/dev.js
@@ -1,6 +1,6 @@
 const args = require('minimist')(process.argv.slice(2));
 const { build } = require('esbuild');
-const {resolve} = require('path');
+const { resolve} = require('path');
 // minimist 是用来解析命令行参数的
 console.log(args);
 const target = args._[0] || 'reactivity';
@@ -17,10 +17,10 @@ const outfile = resolve(__dirname, `../packages/${target}/dist/${target}.${forma
 build({
     entryPoints: [resolve(__dirname, `../packages/${target}/src/index.ts`)],
     outfile,
-    bundle: true,
+    bundle: true, // 把所有的打包在一起
     sourcemap: true,
     format: outputFormat,
-    globalName: pkg.buildOptions?.name,
+    globalName: pkg.buildOptions?.name, // 打包的全局的名字
     platform: format === 'cjs' ? 'node' : 'browser',
     watch: {
         onRebuild(error) {
