45cd774cdc18d9e15351ea1c752709ee72b4ec1d
JaniceZhao22 <janicezhao22@163.com>
2022-05-17 19:21:28 +0800

    feat: 实现构建流程

--- scripts/dev.js
+++ scripts/dev.js
@@ -2,7 +2,6 @@ const args = require('minimist')(process.argv.slice(2));
 const { build } = require('esbuild');
 const { resolve} = require('path');
 // minimist 是用来解析命令行参数的
-console.log(args);
 const target = args._[0] || 'reactivity';
 const format = args.f || 'global';
 
@@ -11,9 +10,10 @@ const pkg = require(resolve(__dirname, `../packages/${target}/package.json`))
 // iife 立即执行函数 （function(){})()
 // cjs node 中的模块 commonJs  module.exports
 // esm 浏览器中的es6 模块  import 
-const outputFormat = format.startswith('global')? 'iife' : format === 'cjs' ? 'cjs'  : 'esm';
+const outputFormat = format.startsWith('global')? 'iife' : format === 'cjs' ? 'cjs'  : 'esm';
 const outfile = resolve(__dirname, `../packages/${target}/dist/${target}.${format}.js`);
 
+// 本身就支持ts
 build({
     entryPoints: [resolve(__dirname, `../packages/${target}/src/index.ts`)],
     outfile,
