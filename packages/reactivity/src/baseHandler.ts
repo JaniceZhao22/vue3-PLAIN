dfedfb689cf636a6810f6a46f612b179549e33b6
JaniceZhao22 <janicezhao22@163.com>
2022-05-18 23:19:19 +0800

    feat： 实现依赖收集原理

--- packages/reactivity/src/baseHandler.ts
+++ packages/reactivity/src/baseHandler.ts
@@ -1,3 +1,5 @@
+import { track, trigger } from './effect'
+
 export const enum ReactiveFlags {
     IS_REACTIVE = '__v_isReactive'
 }
@@ -7,11 +9,18 @@ export const mutableHandlers = {
         if(key === ReactiveFlags.IS_REACTIVE) {
             return true;
         }
+        // 收集依赖
+        track(target, 'get', key);
         // 这里可以监控到用户取值了
         return Reflect.get(target, key, receiver); // 改变this指向， receiver是指proxy
     },
     set(target, key, value, receiver) {
+        let oldValue = target[key];
+        let result = Reflect.set(target, key, value, receiver);
+        if(oldValue !== value) { // 值变化了，要更新
+            trigger(target, 'set', key, value, oldValue);
+        }
         // 这里可以监控到用户设值了
-        return Reflect.set(target, key, value, receiver);
+        return result
     }
 }
--- packages/reactivity/src/effect.ts
+++ packages/reactivity/src/effect.ts
@@ -1,30 +1,73 @@
 
 export let activeEffect = undefined;
-class ReactiveEffect{
+class ReactiveEffect {
+    // 增加一个父结点指向
+    public parent = null;
+    public deps = [];
     // 在实例上新增一个active属性
     public active = true ; // 这个effect默认是激活的。
-    constructor(public fn){  // ts public 表示用户传递的参数也会在this上，相当于this.fn = fn;
+    constructor(public fn){}  // ts public 表示用户传递的参数也会在this上，相当于this.fn = fn;
         
-    }
     run() { // 就是执行effect
         if(!this.active){ // 非激活的状态，只执行，不需要依赖收集
             this.fn()
         }
         // 其余就要依赖收集了 核心就是当前的effect 和稍后渲染的属性关联;
         try{ 
+            this.parent = activeEffect;
             activeEffect = this;
             return this.fn();
         } finally{
-            activeEffect = undefined;
+            activeEffect = this.parent;
+            this.parent = null;
         }
-        
     }
 }
-
-
 
 export function effect(fn) { 
     // 可以根据状态变化，重新执行， effect可以嵌套着写
     const  _effect = new ReactiveEffect(fn);
     _effect.run();
+}
+
+
+// 数据结构
+/**
+ * 对象   某个属性 -》 多个effect
+ * {对象：{name: []}}
+ * WeakMap = {对象: Map{name: Set}}
+ * 
+ * 多对多
+ * 
+ */
+const targetMap = new WeakMap();
+export function track(target, type, key) {
+    if(!activeEffect) return;
+    let depsMap = targetMap.get(target);
+    if(!depsMap){
+        targetMap.set(target, (depsMap = new Map()))
+    }
+    let dep = depsMap.get(key);
+    if(!dep) {
+        depsMap.set(key, (dep = new Set()))
+    }
+    let shouldTrack = !dep.has(activeEffect); // 去重一下
+    if(shouldTrack) {
+        dep.add(activeEffect);
+        // 存放的是属性对应的set
+        activeEffect.deps.push(dep);// 多对多，让effect记录对应的dep， 稍后清理时会用
+    }
+}
+
+export function trigger(target, type, key, value, oldValue) {
+    const depsMap = targetMap.get(target);
+    if(!depsMap) return;
+    const effects = depsMap.get(key);
+    effects && effects.forEach(effect =>{
+        if (effect !== activeEffect) { // 避免递归，死循环的情况
+            effect.run()
+        }
+        
+    })
+
 }
